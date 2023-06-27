4ae0070b201faa3173e3e5fae15bffa53a3b19db
Athul k <athulk018@gmail.com>
2023-06-27 17:08:23 +0530

    Implementation of Player spawner script
    created player spawner script and an unity event Script to spawn layer in-order to spawn player corresponding to the mouse click. Implemented A* path finder plugin

---
+++ Assets/PlayerSpawnerEvent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class PlayerSpawnerEvent : MonoBehaviour
+{
+    public UnityEvent teamBlueSpawnEvent;
+    public UnityEvent teamRedSpawnEvent;
+    public Vector3 leftMouseClickPosition;
+    public Vector3 rightMouseClickPosition;
+    public Camera mainCam;
+    public GameObject teamA;
+    public GameObject teamB;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            leftMouseClickPosition = Input.mousePosition;
+            Ray ray = mainCam.ScreenPointToRay(leftMouseClickPosition);
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                leftMouseClickPosition = hit.point;
+            }
+            teamBlueSpawnEvent?.Invoke();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            rightMouseClickPosition = Input.mousePosition;
+            Ray ray = mainCam.ScreenPointToRay(rightMouseClickPosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                rightMouseClickPosition = hit.point;
+            }
+            teamRedSpawnEvent?.Invoke();
+        }
+
+    }
+}
